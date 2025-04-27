c202b5dc73b4c3be81f90811aee780e054425f1c
Marios Tsoukis <mt5656@nyu.edu>
2025-04-27 02:51:33 -0400

    Add project README and implement scraper

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using ON_Operas
 using Test
+using ON_Operas
+using DataFrames
 
-@testset "ON_Operas.jl" begin
-    # Write your tests here.
+@testset "Basic scrape" begin
+    url = "https://exhibits.stanford.edu/operadata/catalog?f%5Bcountry_ssim%5D%5B%5D=Italy&per_page=96&search_field=composer"
+    df  = scrape_operas([url]; pages=1)
+    @test nrow(df) > 0
+    @test "Opera_Name" in names(df)
 end
